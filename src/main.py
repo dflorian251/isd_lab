ad8ee2c5b95532a8c56457a64672170b28abdd66
Florian Dima <dflorian251@gmail.com>
2024-04-25 08:42:39 +0300

    Citations file fixed manually.
    Manually because there are only 4 searches left in my SerpApi account.
    Last problem to be solved is the editing of the file; simply must put all the file's content in an array

--- src/main.py
+++ src/main.py
@@ -4,18 +4,13 @@ import json
 from dotenv import load_dotenv
 import yaml
 import os
+import requests
 
 
 
-def get_member_scholar(author_id, api_key, start_param):
-	params = {
-	  "api_key": api_key,
-	  "engine": "google_scholar_author",
-	  "hl": "en",
-	  "author_id": author_id,
-	  "start": start_param
-	}
-	search = GoogleSearch(params)
+def get_member_scholar(parameters, start):
+	parameters["start"] = start
+	search = GoogleSearch(parameters)
 	results = search.get_dict()
 	return results
 
@@ -42,6 +37,11 @@ def get_member_name(search_id):
 	    
 if __name__ == "__main__":
 	authors_ids = get_members_ids()
+	
+	load_dotenv()
+	API_KEY = os.getenv("SARPAPI_API_KEY")
+
+
 	for author_id in authors_ids:
 		if (author_id == None):
 			continue
@@ -53,17 +53,18 @@ if __name__ == "__main__":
 		author = [
 				{"author_name": author_name}
 		]
-		data = [author, result["publications"]]
-		data = json.dumps(data, indent=4)
-		data = f"{data},N/N" # N/N indicates that there should be start a newline. Couldn't in a different way start a newline :/
-		data = data.replace("N/N", '\n')
-		with open(f"./assets/publications.json", "a+") as file:
-			file.write(data)
-			file.close()
+		# data = [author, result["publications"]]
+		# data = json.dumps(data, indent=4)
+		# data = f"{data},N/N" # N/N indicates that there should be start a newline. Couldn't in a different way start a newline :/
+		# data = data.replace("N/N", '\n')
+		# with open(f"./assets/publications.json", "a+") as file:
+		# 	file.write(data)
+		# 	file.close()
 
 
 		# Calculate how many pages need to be fetched
 		num_publications = len(result["publications"])
+		print(num_publications)
 		if num_publications <= 20:
 			start_param = 0
 		else :
@@ -71,35 +72,50 @@ if __name__ == "__main__":
 			if num_publications % 100 != 0 :
 				start_param += 1
 
+		params = {
+			"api_key": API_KEY,
+			"engine": "google_scholar_author",
+			"hl": "en",
+			"author_id": author_id,
+		}
+
 		# Write the publications in citation friendly format
 		# This code was added because the page needs to display the publications of each author in APA format
-		load_dotenv()
-		api_key = os.getenv("SARPAPI_API_KEY")
-		result = get_member_scholar(author_id, api_key, start_param)
-		data = [author, result["articles"]]
+		result = get_member_scholar(params, start_param)
+		data = result["articles"]
+		count = 0
+		while count <= start_param:
+			next_url = result["serpapi_pagination"]["next"]
+			result = requests.get(next_url, params).json()
+			data = data + result["articles"]
+			print(json.dumps(data, indent=4))
+			count += 1
+		
+		data = [author, data]
 		data = json.dumps(data, indent=4)
 		data = f"{data},N/N" # N/N indicates that there should be start a newline. Couldn't in a different way start a newline :/
 		data = data.replace("N/N", '\n')
 		with open("./_data/citations.json", "a+") as file:
 			file.write(data)
 			file.close()
+		
 
 
 
 
-	# EDIT THE PUBLICATIONS FILE
-	with open(f"./assets/publications.json", "a+") as file:
-		file.seek(0) # Because in a+ mode the cursor is by default at the end of the file
-		contents = file.read()
-		file.close()
+	# # EDIT THE PUBLICATIONS FILE
+	# with open(f"./assets/publications.json", "a+") as file:
+	# 	file.seek(0) # Because in a+ mode the cursor is by default at the end of the file
+	# 	contents = file.read()
+	# 	file.close()
 	
-	contents = f"[{contents}]"
-	contents = contents[:len(contents)-3] + contents[len(contents)-3+1:] # Remove the last comma for syntax reasons
-	json_contents = json.dumps(json.loads(contents), indent=4)
-	# Use of w+ mode because we don't want the previous file's content
-	with open(f"./assets/publications.json", "w+") as file:
-		file.write(json_contents)
-		file.close()
+	# contents = f"[{contents}]"
+	# contents = contents[:len(contents)-3] + contents[len(contents)-3+1:] # Remove the last comma for syntax reasons
+	# json_contents = json.dumps(json.loads(contents), indent=4)
+	# # Use of w+ mode because we don't want the previous file's content
+	# with open(f"./assets/publications.json", "w+") as file:
+	# 	file.write(json_contents)
+	# 	file.close()
 
 	# EDIT THE CITATIONS FILE
 	with open(f"./_data/citations.json", "a+") as file:
@@ -110,6 +126,7 @@ if __name__ == "__main__":
 	contents = f"[{contents}]"
 	contents = contents[:len(contents)-3] + contents[len(contents)-3+1:] # Remove the last comma for syntax reasons
 	json_contents = json.dumps(json.loads(contents), indent=4)
+	print(json_contents)
 	# Use of w+ mode because we don't want the previous file's content
 	with open(f"./_data/citations.json", "w+") as file:
 		file.write(json_contents)
