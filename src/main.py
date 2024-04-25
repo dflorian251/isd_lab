27555203868dcfd39597a01dca51fdd7a1e1d605
Florian Dima <dflorian251@gmail.com>
2024-04-25 09:22:26 +0300

    Having troubles with the Python script.
    For some reason, the file editing isn't happening at all

--- src/main.py
+++ src/main.py
@@ -48,23 +48,24 @@ if __name__ == "__main__":
 
 		# Write the publications
 		author = scholarly.search_author_id(author_id)
+		author_interests = author["interests"]
 		result = scholarly.fill(author, sections=["publications"])
 		author_name = get_member_name(author_id)
 		author = [
-				{"author_name": author_name}
+				{"author_name": author_name},
+				{"interest": author_interests}
 		]
-		# data = [author, result["publications"]]
-		# data = json.dumps(data, indent=4)
-		# data = f"{data},N/N" # N/N indicates that there should be start a newline. Couldn't in a different way start a newline :/
-		# data = data.replace("N/N", '\n')
-		# with open(f"./assets/publications.json", "a+") as file:
-		# 	file.write(data)
-		# 	file.close()
+		data = [author, result["publications"]]
+		data = json.dumps(data, indent=4)
+		data = f"{data},N/N" # N/N indicates that there should be start a newline. Couldn't in a different way start a newline :/
+		data = data.replace("N/N", '\n')
+		with open(f"./assets/publications.json", "a+") as file:
+			file.write(data)
+			file.close()
 
 
 		# Calculate how many pages need to be fetched
 		num_publications = len(result["publications"])
-		print(num_publications)
 		if num_publications <= 20:
 			start_param = 0
 		else :
@@ -88,34 +89,37 @@ if __name__ == "__main__":
 			next_url = result["serpapi_pagination"]["next"]
 			result = requests.get(next_url, params).json()
 			data = data + result["articles"]
-			print(json.dumps(data, indent=4))
 			count += 1
 		
 		data = [author, data]
 		data = json.dumps(data, indent=4)
 		data = f"{data},N/N" # N/N indicates that there should be start a newline. Couldn't in a different way start a newline :/
 		data = data.replace("N/N", '\n')
-		with open("./_data/citations.json", "a+") as file:
-			file.write(data)
-			file.close()
+		# with open("./_data/citations.json", "a+") as file:
+		# 	file.write(data)
+		# 	file.close()
 		
+	
 
-
+	print("Loop finished")
 
 
 	# # EDIT THE PUBLICATIONS FILE
-	# with open(f"./assets/publications.json", "a+") as file:
-	# 	file.seek(0) # Because in a+ mode the cursor is by default at the end of the file
-	# 	contents = file.read()
-	# 	file.close()
-	
-	# contents = f"[{contents}]"
-	# contents = contents[:len(contents)-3] + contents[len(contents)-3+1:] # Remove the last comma for syntax reasons
-	# json_contents = json.dumps(json.loads(contents), indent=4)
-	# # Use of w+ mode because we don't want the previous file's content
-	# with open(f"./assets/publications.json", "w+") as file:
-	# 	file.write(json_contents)
-	# 	file.close()
+	print("Getting ready to edit the file")
+	with open(f"./assets/publications.json", "a+") as file:
+		print('File opened for reading')
+		file.seek(0) # Because in a+ mode the cursor is by default at the end of the file
+		contents = file.read()
+		file.close()
+
+	contents = f"[{contents}]"
+	contents = contents[:len(contents)-3] + contents[len(contents)-3+1:] # Remove the last comma for syntax reasons
+	json_contents = json.dumps(json.loads(contents), indent=4)
+	# Use of w+ mode because we don't want the previous file's content
+	with open(f"./assets/publications.json", "w+") as file:
+		print("File opened for writing")
+		file.write(json_contents)
+		file.close()
 
 	# EDIT THE CITATIONS FILE
 	with open(f"./_data/citations.json", "a+") as file:
